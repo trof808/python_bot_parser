06de66b44b0e7773301ca65dd321477abdf5295a
trof <reatrof@gmail.com>
2017-05-01 00:39:54 +0300

    new buttons

--- bot.py
+++ bot.py
@@ -5,27 +5,74 @@ import config
 
 URL = 'https://www.avito.ru/krasnodar'
 
+custom_request = {}
+
 buttons = {
     'Квартиры': '/kvartiry',
-    'Дома': '/doma_dachi_kottedzhi'
+    'Дома': '/doma_dachi_kottedzhi',
+    'Комнаты': '/komnaty',
+    'Коммерция': '/kommercheskaya_nedvizhimost',
+    'Купить': '/prodam',
+    'Снять': '/sdam'
 }
 
 TOKEN = config.TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+
+##Функция, которая обрабатывает команду /start
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*[types.KeyboardButton(name) for name in ['Квартиры', 'Дома']])
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Квартиры', 'Дома', 'Комнаты', 'Коммерция']])
     sent = bot.send_message(message.chat.id, 'Выберите тип недвижимости', reply_markup=keyboard)
 
-    bot.register_next_step_handler(sent, hello)
+    ##После отправки сообщения, бот ждет сообщение в ответ, чтобы вызвать функцию stepTwo()
+    bot.register_next_step_handler(sent, stepTwo)
 
-def hello(message):
+
+##Обработка любых пользовательских сообщений
+# @bot.message_handler(content_types=["text"])
+# def handle_message(message):
+#     bot.send_message(
+#         message.chat.id,
+#         'Для того, чтобы составить запрос на поиск нужной недвижимости введите команду /start'
+#     )
+
+
+
+def stepTwo(message):
     msg = message.text
-    url = URL + buttons[msg]
-    result = avito.parse(avito.get_html(url))[:10]
+    custom_request['1'] = msg
+    custom_url = URL + buttons[msg]
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Купить', 'Снять', 'Показать объявления']])
+    sent = bot.send_message(message.chat.id, 'Что вы хотите?', reply_markup=keyboard)
+
+    bot.register_next_step_handler(sent, stepThree)
+
+
+
+def stepThree(message):
+    if(message.text == 'Показать объявления'):
+        showResults(message)
+
+def showResults(message):
+    info_msg = 'По Вашему запросу: \n'
+    for item in custom_request:
+        info_msg = info_msg + item + ': ' + custom_request[item] + '\n'
+        custom_url = URL + buttons[custom_request[item]]
+
+    info_msg = info_msg + 'Было найдено'
+
+    bot.send_message(
+        message.chat.id,
+        info_msg
+    )
+
+    result = avito.parse(avito.get_html(custom_url))[:10]
 
     for item in result:
         bot.send_message(
@@ -33,5 +80,5 @@ def hello(message):
             '{price} \n {link}'.format(price=item['price'], link=item['link'])
         )
 
-
-bot.polling()
+if __name__ == '__main__':
+    bot.polling()
